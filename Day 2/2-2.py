958585f5b98a17afd77168c09b0eceb7c4315f54
Logan Waite <logan@itovi.com>
2017-12-06 16:32:50 -0700

    finished day 2

--- Day 2/2-2.py
+++ Day 2/2-2.py
@@ -1,5 +1,16 @@
 def Checksum(data):
-    rows = data.split("\n")
+	rows = data.split("\n")
+	divided_list = list()
+
+	for row in rows:
+		numbers = map(int, row.split("\t"))
+
+		for number in numbers:
+			array = [number / x for x in numbers if (number % x == 0) and (number != x)]
+			if len(array) > 0:
+				divided_list.append(array[0])
+
+	print(sum(divided_list))
 
 file_object = open("2.txt", "r")
 data = file_object.read()
