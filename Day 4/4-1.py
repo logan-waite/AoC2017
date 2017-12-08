848abbd942a9ba78c3f2732ccaec2a5995d3841c
Logan Waite <logan@itovi.com>
2017-12-08 14:32:11 -0700

    Finish Day 4

--- Day 4/4-1.py
+++ Day 4/4-1.py
@@ -1,5 +1,13 @@
 def MainFunction(data):
-	pass
+	phrases = data.split("\n")
+	valid = 0
+
+	for phrase in phrases:
+		words = phrase.split(" ")
+		if len(words) == len(set(words)):
+			valid += 1
+
+	print(valid)
 
 file_object = open("4.txt", "r")
 data = file_object.read()
--- Day 4/4-2.py
+++ Day 4/4-2.py
@@ -1,8 +1,21 @@
 def MainFunction(data):
-	pass
+	phrases = data.split("\n")
+	valid = 0
+
+	for phrase in phrases:
+		words = phrase.split(" ")
+		ordered_words = list()
+		for word in words:
+			letters = sorted(list(word))
+			new_word = "".join(letters)
+			ordered_words.append(new_word)
+		if len(ordered_words) == len(set(ordered_words)):
+				valid += 1
 
 
-file_object = open("5.txt", "r")
+	print(valid)
+
+file_object = open("4.txt", "r")
 data = file_object.read()
 
 MainFunction(data)
